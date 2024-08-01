f13040726a6f8e6f27bf6740d198fcd4600e8998
Jesus <amjo220898@upemor.edu.mx>
2024-08-01 14:49:48 -0600

    Initial commit

---
+++ app/src/main/java/com/example/solucionesparaplagas/welcome_activity.java
@@ -0,0 +1,70 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		welcome
+	 *	@date 		Thursday 01st of August 2024 06:55:20 PM
+	 *	@title 		Pgina de inicio
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.solucionesparaplagas;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+	public class welcome_activity extends Activity {
+
+	
+	private View _bg__welcome_ek2;
+	private ImageView marcoimagenp;
+	private ImageView textsolucionesparaplagas;
+	private TextView label_text;
+	private TextView soluciones_para_plagas_urbanas_y_agr_colas;
+	private TextView __bienvenido_;
+	private ImageView logotipsinfondo;
+	private View informacion;
+	private TextView bajada_a_chapultepec___3__chapultepec__62360_cuernavaca__mor_;
+	private ImageView iconodireccion;
+	private ImageView iconopaginawebrecortada_1;
+	private TextView solucionesparaplagas_com;
+	private TextView crear_una_cuenta;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.welcome);
+
+		
+		_bg__welcome_ek2 = (View) findViewById(R.id._bg__welcome_ek2);
+		marcoimagenp = (ImageView) findViewById(R.id.marcoimagenp);
+		textsolucionesparaplagas = (ImageView) findViewById(R.id.textsolucionesparaplagas);
+		label_text = (TextView) findViewById(R.id.label_text);
+		soluciones_para_plagas_urbanas_y_agr_colas = (TextView) findViewById(R.id.soluciones_para_plagas_urbanas_y_agr_colas);
+		__bienvenido_ = (TextView) findViewById(R.id.__bienvenido_);
+		logotipsinfondo = (ImageView) findViewById(R.id.logotipsinfondo);
+		informacion = (View) findViewById(R.id.informacion);
+		bajada_a_chapultepec___3__chapultepec__62360_cuernavaca__mor_ = (TextView) findViewById(R.id.bajada_a_chapultepec___3__chapultepec__62360_cuernavaca__mor_);
+		iconodireccion = (ImageView) findViewById(R.id.iconodireccion);
+		iconopaginawebrecortada_1 = (ImageView) findViewById(R.id.iconopaginawebrecortada_1);
+		solucionesparaplagas_com = (TextView) findViewById(R.id.solucionesparaplagas_com);
+		crear_una_cuenta = (TextView) findViewById(R.id.crear_una_cuenta);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
